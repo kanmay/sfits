d38161c03698cc68bbf69ac6033670a7869fc826
kanmay <kanmaysinghal@gmail.com>
2018-02-09 15:33:22 +0530

    third commit

--- Controller/db.php
+++ Controller/db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "85.10.205.173:3307";
-$username = "mayank";
-$password = "mayank";
-$dbname = "sfits";
+$servername = "ec2-23-21-162-90.compute-1.amazonaws.com:5432";
+$username = "aqqqqsiwerdxed";
+$password = "8ac3cdad79fcca7d1af33e38e4f192c61da1b2ee24676d08da62478dc7d4a6c7";
+$dbname = "deb90cpmmdagnp";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
